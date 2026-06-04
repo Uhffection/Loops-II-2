32100cd4535f48800800e71cd1a6fff061e8e2e6
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-06-04 21:51:40 +0000

    Create function fizzbuzz using for loop and while loop

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 // Write your code here
+function fizzbuzz() {
+    for (let i = 1; i <= 20; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzbuzz()
+
+// Code done with while loop (for my own practice)
+
+function fizzbuzz2() {
+    let i = 1
+
+    while (i <= 20) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else {
+            console.log(i)
+        }
+        i++
+    }
+}
+
+fizzbuzz2()
